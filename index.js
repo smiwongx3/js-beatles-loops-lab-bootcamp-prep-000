05d4b8a23cbf51ca10441e28fc11c682d81b6f7e
Samantha Wong <smiwong920@gmail.com>
2019-01-27 01:58:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+  var arraySentences=[]
+ var i
+  for (i = 0; i < arrayMusicians.length; i++) { 
+  arraySentences.push(arrayMusicians[i] + " plays " + arrayInstruments[i])
+}
+return arraySentences
+}
+
+function johnLennonFacts(arrayFacts) {
+  var arrayExclamations=[]
+  let x = 0
+    while (x < arrayFacts.length) {
+  arrayExclamations.push(arrayFacts[x] + "!!!")
+  x++;
+}
+return arrayExclamations
+}
+
+function iLoveTheBeatles(number) {
+  var arrayEmpty=[]
+  
+  do {arrayEmpty.push("I love the Beatles!")} 
+  while (number<15)
+}
+  return arrayEmpty
+}
